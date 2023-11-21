0a4b24898b4a5e4f6750dd0eed26018020146957
sidneyv7 <hadi.kootzadeh@gmail.com>
2023-11-22 01:27:10 +0330

    List Category

---
+++ BuklyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BuklyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuklyWeb.Controllers
+{
+  public class CategoryController : Controller
+  {
+    private readonly BulkyContext _context;
+
+        public CategoryController(BulkyContext context)
+        {
+      _context = context;            
+
+        }
+        public IActionResult Index()
+    {
+
+      return View(_context.Categories.ToList());
+
+    }
+
+
+
+  }
+}
