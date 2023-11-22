e47e3073a6a7a75c1375bcb54ee8e72777329654
sidneyv7 <hadi.kootzadeh@gmail.com>
2023-11-22 04:27:47 +0330

    updatedb

--- Bukly.DataAcess/Data/BulkyContext.cs
+++ Bukly.DataAcess/Data/BulkyContext.cs
@@ -21,7 +21,12 @@ public partial class BulkyContext : DbContext
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        OnModelCreatingPartial(modelBuilder);
+    modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+    OnModelCreatingPartial(modelBuilder);
     }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
