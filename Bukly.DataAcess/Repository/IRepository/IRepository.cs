3d36178e467bc3491443e1a81879ec4382b2073a
sidneyv7 <hadi.kootzadeh@gmail.com>
2023-11-22 03:39:09 +0330

    Irepository

---
+++ Bukly.DataAcess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bukly.DataAcess.Repository.IRepository
+{
+  internal interface IRepository <T> where T : class
+  {
+    IEnumerable<T> GetAll ();
+    T Get(Expression<Func<T,bool>> filter);
+    
+    void Delete (T entity);
+    void Add (T entity);
+    void DeleteAll(IEnumerable<T> entities);
+  }
+}
